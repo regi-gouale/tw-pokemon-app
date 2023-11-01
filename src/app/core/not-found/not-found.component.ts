897ac8425a6fe1c2e931f8d5ab8fe57e4682b09b
Régi GOUALE <74569854+regi-gouale@users.noreply.github.com>
2023-11-01 22:58:57 +0100

    Create not found page

---
+++ src/app/core/not-found/not-found.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-not-found',
+  templateUrl: './not-found.component.html',
+  standalone: true,
+  imports: [RouterLink],
+})
+export class NotFoundComponent {
+
+}
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,26 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
-
 import { importProvidersFrom } from '@angular/core';
 import { AppComponent } from './app/app.component';
+import { provideRouter, Routes } from '@angular/router';
+import { withInterceptorsFromDi, provideHttpClient } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
+
+const routes: Routes = [
+    { path: '', redirectTo: '/pokemons', pathMatch: 'full' },
+    {
+        path: '**',
+        loadComponent: () => import("./app/core/not-found/not-found.component")
+            .then(m => m.NotFoundComponent)
+    }
+];
+
 
 
 bootstrapApplication(AppComponent, {
-    providers: [importProvidersFrom(BrowserModule)]
+    providers: [
+        provideHttpClient(withInterceptorsFromDi()),
+        importProvidersFrom(BrowserModule, FormsModule),
+        provideRouter(routes)
+    ]
 })
   .catch(err => console.error(err));
