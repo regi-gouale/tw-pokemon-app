942be39bf4200b0d9617b3cd614337ec254fb0a5
Régi GOUALE <74569854+regi-gouale@users.noreply.github.com>
2023-11-01 21:51:41 +0100

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import { NgSwitch, NgSwitchDefault, NgSwitchCase } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    standalone: true,
+    imports: [RouterOutlet]
 })
 export class AppComponent {
-  title = 'tw-pokemon-app';
+  title = 'Pokédex';
 }
